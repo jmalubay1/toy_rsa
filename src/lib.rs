f573796a5fa4c0ccdd772fbbd88f32600eb62aed
jmalubay1 <jmalubay@babbage.cs.pdx.edu>
2021-04-21 12:47:49 -0700

    Keygen and encrypt done, still needs testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 ///! RSA encryption library
 ///!
 ///! Jordan Malubay 2021
-
 use toy_rsa_lib::*;
 
 /// Fixed RSA encryption exponent.
@@ -14,26 +13,28 @@ pub const EXP: u64 = 65537;
 /// `p` `q` and testing that they satisfy `λ(pq) <= EXP` and
 /// that `λ(pq)` has no common factors with `EXP`.
 pub fn genkey() -> (u32, u32) {
-  loop {
-    let p: u32 = rsa_prime();
-    let q: u32 = rsa_prime();
-    let lam: u32 = lcm(p-1,q-1);
-    if EXP < lam && gcd(EXP,lam) == 1 { break }
-  }
-  (p,q)
+    let p: u32;
+    let q: u32;
+    loop {
+        p = rsa_prime();
+        q = rsa_prime();
+        let lam: u64 = lcm((p as u64) - 1, (q as u64) - 1);
+        if EXP < lam && gcd(EXP, lam) == 1 {
+            break;
+        }
+    }
+    (p, q)
 }
 
 /// Encrypt the plaintext `msg` using the RSA public `key`
 /// and return the ciphertext.
 pub fn encrypt(key: u64, msg: u32) -> u64 {
-
+  modexp(msg as u64,EXP,key)
 }
 
 /// Decrypt the cipertext `msg` using the RSA private `key`
 /// and return the resulting plaintext.
-pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
-
-}
+pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {}
 
 #[cfg(test)]
 mod tests {
