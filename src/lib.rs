6ce1a214275b5f8d7b2269677dae8a3b1218f542
jmalubay1 <jmalubay@babbage.cs.pdx.edu>
2021-04-21 13:19:50 -0700

    All done, need to build tests and example

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 ///! RSA encryption library
 ///!
 ///! Jordan Malubay 2021
+use std::convert::TryInto;
 use toy_rsa_lib::*;
 
 /// Fixed RSA encryption exponent.
 pub const EXP: u64 = 65537;
+
+///λ(pq) returns the LCM of p-1 and q-1
+pub fn lambda(p: u32, q: u32) -> u64 {
+    lcm((p as u64) - 1, (q as u64) - 1)
+}
 
 /// Generate a pair of primes in the range `2**30..2**31`
 /// suitable for RSA encryption with exponent
@@ -13,12 +19,12 @@ pub const EXP: u64 = 65537;
 /// `p` `q` and testing that they satisfy `λ(pq) <= EXP` and
 /// that `λ(pq)` has no common factors with `EXP`.
 pub fn genkey() -> (u32, u32) {
-    let p: u32;
-    let q: u32;
+    let mut p: u32;
+    let mut q: u32;
     loop {
         p = rsa_prime();
         q = rsa_prime();
-        let lam: u64 = lcm((p as u64) - 1, (q as u64) - 1);
+        let lam: u64 = lambda(p, q);
         if EXP < lam && gcd(EXP, lam) == 1 {
             break;
         }
@@ -29,17 +35,26 @@ pub fn genkey() -> (u32, u32) {
 /// Encrypt the plaintext `msg` using the RSA public `key`
 /// and return the ciphertext.
 pub fn encrypt(key: u64, msg: u32) -> u64 {
-  modexp(msg as u64,EXP,key)
+    modexp(msg as u64, EXP, key)
 }
 
 /// Decrypt the cipertext `msg` using the RSA private `key`
 /// and return the resulting plaintext.
-pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {}
+pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
+    let d: u64 = modinverse(EXP, lambda(key.0, key.1));
+    modexp(msg as u64, d, (key.0 * key.1) as u64)
+        .try_into()
+        .unwrap()
+}
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let key = genkey();
+        let msg: u32 = 123456789;
+        let cipher: u64 = encrypt(key.0, msg);
+        let deciph: u32 = decrypt(key, cipher);
+        assert_eq!(msg, deciph);
     }
 }
